86205e77b6bdd86876d3cd2f8da965b23c804028
Damian <damianjnc@gmail.com>
2021-01-31 04:01:45 +0000

    mikroorm setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world')
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants"
+import { Post } from "./entities/Post"
+import mikroConfig from "./mikro-orm.config"
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig)
+
+  const post = orm.em.create(Post, { title: "my first post" })
+  await orm.em.persistAndFlush(post)
+  console.log("-------sql 2 -------")
+  await orm.em.nativeInsert(Post, { title: "my second post" })
+}
+
+main().catch((err) => {
+  console.error(err)
+})
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,10 @@
+import { Post } from "./entities/Post"
+import { __prod__ } from "./constants"
+import { MikroORM } from "@mikro-orm/core"
+
+export default {
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__
+} as Parameters<typeof MikroORM.init[0]>
